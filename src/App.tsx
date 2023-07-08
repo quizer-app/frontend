e893d7939c4fd2453633a0149e423f1b31bde794
SzymonBudziak <szymonbudziakj@gmail.com>
2023-07-08 18:32:10 +0200

    elo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import * as React from 'react'
-import * as auth from 'auth-provider'
-import { BrowserRouter as Router } from 'react-router-dom'
-
-
+import * as React from "react";
+import * as auth from "auth-provider";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
-    
-  const isSuccess = false
-  
-  
-  if (isSuccess) {
-    const props = {user, login, register, logout}
-    return user ? (
-      <Router>
-        
-      </Router>
-    )
-  } 
-  
-  
-  return <div>App</div>;
+    const isSuccess = false;
+
+    // if (isSuccess) {
+    //   const props = {user, login, register, logout}
+    //   return user ? (
+    //     <Router>
+
+    //     </Router>
+    //   )
+    // }
+
+    return <div>App</div>;
 }
