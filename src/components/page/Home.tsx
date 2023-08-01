5b2d4858f8a6e54c482797c2ed3b55d447751705
SzymonBudziak <szymonbudziakj@gmail.com>
2023-08-01 19:13:19 +0200

    main page

--- src/components/page/Home.tsx
+++ src/components/page/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-	return (
-		<>
-			<button className="btn">Button</button>
-		</>
-	);
+  return (
+    <>
+      <div className="bg-primary absolute top-0 right-0 w-full h-[300vh] -z-50"></div>
+    </>
+  )
 }
