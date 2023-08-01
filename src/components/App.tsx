591baa279627cb33a6218e46e1948509441015d9
EloToJa <elotoja@protonmail.com>
2023-08-01 19:44:01 +0200

    Update Routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./page/Home";
-import Login from "./page/Login";
-import Register from "./page/Register";
 import SignIn from "./page/SignIn";
+import Test from "./page/Test";
 import Users from "./page/Users";
 import RequireAuth from "./router/RequireAuth";
 import NotFound from "./status/NotFound";
-import Unauthorized from "./status/Unauthorized";
 
 export default function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
-				<Route path="login" element={<Login />} />
-				<Route path="register" element={<Register />} />
-				<Route path="unauthorized" element={<Unauthorized />} />
 				<Route path="/" element={<Home />} />
 				<Route path="signin" element={<SignIn />} />
+
+				<Route path="test" element={<Test />} />
+
 				<Route element={<RequireAuth />}>
-					<Route path="users" element={<Users />} />
+					<Route path="users" element={<Users />} />R
 				</Route>
 
 				<Route path="*" element={<NotFound />} />
