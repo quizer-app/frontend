0423c11371d25229c52fa001e604837e1059836e
SzymonBudziak <szymonbudziakj@gmail.com>
2024-01-09 01:20:48 +0100

    query error

--- src/components/layout/Sections/TileGrid/TileGrid.tsx
+++ src/components/layout/Sections/TileGrid/TileGrid.tsx
@@ -1,10 +1,12 @@
 import QuizTile from "./QuizTile";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/api/axios";
 
-// interface QuizesQueryProps {
-//   userName: string;
-//   name: string;
-//   description: string;
-// }
+interface QuizesQueryProps {
+  userName: string;
+  name: string;
+  description: string;
+}
 
 export default function TileGrid() {
   const quizes = [
@@ -46,25 +48,15 @@ export default function TileGrid() {
     },
   ];
 
-  // const { isLoading, isError, error, data } = useQuery({
-  //   queryKey: ["quizes"],
-  //   queryFn: () => api.get<QuizesQueryProps[]>("/api/v1/Quiz"),
-  // });
+  const { isLoading, isError, error, data } = useQuery({
+    queryKey: ["quizes"],
+    queryFn: () => api.get<QuizesQueryProps[]>("/api/v1/Quiz"),
+  });
 
   return (
     <section className="bg-primary w-full flex items-center justify-center py-14 md:py-16 lg:py-20">
       <div className="mainContainer grid grid-cols-1 gap-4 lg:gap-6 md:grid-cols-2 xl:grid-cols-3">
-        {quizes.map((el, id) => {
-          return (
-            <QuizTile
-              name={el.name}
-              userName={el.userName}
-              desc={el.description}
-              key={id}
-            />
-          );
-        })}
-        {/* <div className="text-red-600">
+        <div className="text-red-600">
           {isLoading && "Loading..."}
           {isError && JSON.stringify(error)}
         </div>
@@ -77,7 +69,7 @@ export default function TileGrid() {
               key={id}
             />
           );
-        })} */}
+        })}
       </div>
     </section>
   );
