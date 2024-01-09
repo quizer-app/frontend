62bc811391af36a95b56974e84b85fe8433dba47
EloToJa <elotoja@protonmail.com>
2024-01-09 04:23:10 +0100

    Fix api url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL as string;
+const baseURL =
+  (import.meta.env.VITE_API_URL as string) ?? "https://api.local.elotoja.com";
 
 export const api = axios.create({
   baseURL,
